0582572c925ee513340661bc76b305dbb9c760be
a320ne0 <132751339+a320ne0@users.noreply.github.com>
2023-05-07 09:42:16 +0000

    Alpakas vorherladen

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,14 @@
-
 import 'package:flutter/material.dart';
-const alpakaimages =["alpaka.png","zwinkerndes_Alpaka.png","rotes_alpaka.png","zwinkerndes_rotes_alpaka.png","lilanes_alpaka.png","zwinkerndes_lilanes_alpaka.png"];
+
+const alpakaimages = [
+  "alpaka.png",
+  "zwinkerndes_Alpaka.png",
+  "rotes_alpaka.png",
+  "zwinkerndes_rotes_alpaka.png",
+  "lilanes_alpaka.png",
+  "zwinkerndes_lilanes_alpaka.png"
+];
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -9,47 +17,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int image_index=0;
+  int image_index = 0;
+  List<AssetImage> asset_images = [];
+
+  @override
+  void initState() {
+    super.initState();
+    alpakaimages.forEach((element) {
+      asset_images.add(AssetImage("assets/${element}"));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return (
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF8800ff)
+    return (Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF8800ff)),
+            child: const Padding(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                "START!",
+                style: TextStyle(fontSize: 24),
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(12),
-                child: Text(
-                  "START!",
-                  style: TextStyle(fontSize: 24),
-                ),
-              ),
-              onPressed: () {
-                DefaultTabController.of(context).animateTo(1);
-              },
             ),
-            const SizedBox(height: 50),
-            GestureDetector(
-              onTap:()async{
+            onPressed: () {
+              DefaultTabController.of(context).animateTo(1);
+            },
+          ),
+          const SizedBox(height: 50),
+          GestureDetector(
+              onTap: () async {
                 image_index++;
                 setState(() {});
-                await Future.delayed(const Duration(milliseconds: 100),);
+                await Future.delayed(
+                  const Duration(milliseconds: 150),
+                );
                 image_index++;
                 setState(() {});
               },
               child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 100),
+                  duration: const Duration(milliseconds: 100),
                   child: Image(
-              key: UniqueKey(),
-                      image: AssetImage("assets/${alpakaimages[image_index%alpakaimages.length]}"))),
-            )
-          ],
-        ),
-      )
-    );
+                    key: UniqueKey(),
+                    image: asset_images[image_index % alpakaimages.length],
+                  )))
+        ],
+      ),
+    ));
   }
 }
